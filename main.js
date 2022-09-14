7d0abf227aa93a41f82bf4d17a912cc13f8865db
Zahro <zahro6800@gmail.com>
2022-09-14 18:01:09 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,291 @@
+    // 1-masala
+
+// var a = prompt("Butun son kiriting:")-0;
+// if(a>0){
+//     alert("Musbat son")
+// }
+// else{
+//     alert("Manfiy son")
+// }
+
+    // 2-masala
+
+// var a = prompt("Butun son kiriting:")-0;
+// if(a%2==0){
+//     alert("juft son")
+// }
+// else{
+//     alert("toq son")
+// }
+
+    // 3-masala
+
+// var a = prompt("Butun son kiriting:")-0;
+// if(a%2==0){
+//     alert("juft son")
+// }
+// else{
+//     alert("toq son")
+// }
+
+
+    // 4-masala
+
+// var a = prompt("Birinchi sonni kiriting:")-0;
+// var b = prompt("Ikkinchi sonni kiriting:")-0;
+// if(a>0 && b>0){
+//     alert("Ikkalasi ham musbat son")
+// }
+// else{
+//     alert("Orasida manfiy son ishtirok etgan")
+// }
+
+    // 5-masala
+
+// var a = prompt("Birinchi sonni kiriting:")-0;
+// var b = prompt("Ikkinchi sonni kiriting:")-0;
+// var c = prompt("Uchinchi sonni kiriting:")-0;
+//  if(a>b && b>c){
+//     alert("1-son kotta")
+//  }
+//  else{
+//     alert("false")
+//  }
+
+    // 6-masala
+
+// var a = prompt("Birinchi sonni kiriting:")-0;
+// var b = prompt("Ikkinchi sonni kiriting:")-0;
+// var c = prompt("Uchinchi sonni kiriting:")-0;
+//  if(a>b<c){
+//     alert("b son o`rtada yotadi")
+//  }
+//  else{
+//     alert("false")
+//  }
+
+    // 7-masala
+
+// var a = prompt("Birinchi sonni kiriting:")-0;
+// var b = prompt("Ikkinchi sonni kiriting:")-0;
+// if(a%2==0 && b%2==0 ){
+//     alert("Ikkalasi ham juft son")
+// }else if(!a%2==0 && !b%2==0){
+//     alert("Ikkalasi ham toq son")
+// }
+// else{
+//     alert("false")
+// }
+
+    // 8-masala
+
+// var a = prompt("Birinchi sonni kiriting:")-0;
+// var b = prompt("Ikkinchi sonni kiriting:")-0;
+// var c = prompt("Uchinchi sonni kiriting:")-0;
+// var d = prompt("To`rtinchi sonni kiriting:")-0;
+// if(a>0 && b>0 && c>0 && d>0){
+//     alert("Hammasi musbat son")
+// }
+// else{
+//     alert("Orasida manfiy son ishtirok etgan")
+// }
+
+    // 9-masala
+
+// var a = prompt("Birinchi sonni kiriting:")-0;
+// var b = prompt("Ikkinchi sonni kiriting:")-0;
+// var c = prompt("Uchinchi sonni kiriting:")-0;
+// var d = prompt("To`rtinchi sonni kiriting:")-0;
+// if(a>0 && b>0 && c>0 && d>0){
+//     alert("Hammasi musbat son")
+// }
+// if(a>0 && b>0 || a>0 && c>0 || a>0 && d>0 || b>0 && c>0 || b>0 && d>0 || c>0 && d>0){
+//     alert("Orasida kamida 2 tasi musbat")
+// }
+// else{
+//     alert("2 tadan ko`p manfiy son ishtirok etgan")
+// }
+
+    // 10-masala
+// var a = prompt("Raqam kirgazib hafta kunini bilib oling:")
+// if(a == 1 && a>0 && a<8){
+//     alert("Monday")
+// }else if (a == 2 && a>0 && a<8){
+//     alert("Tuesday")
+// }else if (a == 3 && a>0 && a<8){
+//     alert("Wednesday")
+// }else if (a == 4 && a>0 && a<8){
+//     alert("Thirsday")
+// }else if (a == 5 && a>0 && a<8){
+//     alert("Friday")
+// }else if (a == 6 && a>0 && a<8){
+//     alert("Saturday")
+// }else if (a == 7 && a>0 && a<8){
+//     alert("Sunday")
+// }
+// else{
+//     alert("1 dan 7 gacha raqam kiriting!")
+// }
+
+    // 11-masala
+
+// var a = prompt("Telefon kodini kiritib kompaniyasini bilib oling:")-0;
+// if(a == 99 || a == 95){
+//     alert("Uzmobile")
+// }else if(a == 90 || a == 91){
+//     alert("Beeline")
+// }else if(a == 93 || a == 94){
+//     alert("Ucell")
+// }else if(a == 98){
+//     alert("Perfectum")
+// }else{
+//     alert("Bunday kompaniya mavjud emas!")
+// }
+
+    // 12-masala
+
+// var a = prompt("Son kiriting:")-0;
+// if(a>0){
+//     alert(a+1)
+// }else if(a<0){
+//     alert(a-1)
+// }
+// else{
+//     false
+// }
+
+    // 13-masala
+
+// var a = prompt("Son kiriting:")-0;
+// if(a>0){
+//     alert(a*3)
+// }else if(a<0){
+//     alert(a/2)
+// }
+// else{
+//     false
+// }
+
+    // 14-masala
+
+// alert("2 ta son kiritib eng kattasini toping")
+// var a = prompt("1-sonni kiriting:")-0;
+// var b = prompt("2-sonni kiriting:")-0;
+// if(a>b){
+//     alert("1-son eng kattasi")
+// }
+// else{
+//     alert("2-son eng kattasi")
+// }
+
+    // 15-masala
+
+// alert("3 ta son kiritib eng kattasini toping")
+// var a = prompt("1-sonni kiriting:")-0;
+// var b = prompt("2-sonni kiriting:")-0;
+// var c = prompt("3-sonni kiriting")-0;
+// if(a>b && a>c){
+//     alert("1-son eng kattasi")
+// }else if(b>a && b>c){
+//     alert("2-son eng kattasi")
+// }
+// else{
+//     alert("3-son eng kattasi")
+// }
+
+    // 16-masala
+
+// alert("3 ta son kiritib eng kichigini toping")
+// var a = prompt("1-sonni kiriting:")-0;
+// var b = prompt("2-sonni kiriting:")-0;
+// var c = prompt("3-sonni kiriting")-0;
+// if(a<b && a<c){
+//     alert("1-son eng kichigi")
+// }else if(b<a && b<c){
+//     alert("2-son eng kichigi")
+// }
+// else{
+//     alert("3-son eng kichigi")
+// }
+
+    // 17-masala
+// rasm chiqazishi o`rganmadim hali`
+
+
+    // 18-masala
+
+//  3 == 600000
+//  4 == 800000
+//  5 == 1000000
+
+// var a = prompt("Bahoyizi kiritib stipendiyangizni bilib oling:")-0;
+// if(a>2 && a<6){
+//     alert(a*200000)
+// }
+// else{
+//     alert("Kiritilgan baho (3),(4),(5) bo`lishi kerak")
+// }
+
+    // 19-masala
+// var a = prompt("Son kiriting")-0;
+// if(a%2==0 && a>0){
+//     alert("Kiritilgan son juft musbat")
+// }else if(a%2==0 && a<0){
+//     alert("Kiritilgan son juft manfiy")
+// }else if(!a%2==0 && a>0) {
+//     alert("Kiritilgan son toq musbat")
+// }else if(!a%2==0 && a<0){
+//     alert("Kiritilgan son toq manfiy")
+// }else{
+//     alert("false")
+// }
+
+    // 20-masala
+
+// var a = prompt("Yilizi kiritib yoshizi biling:")-0;
+// if(a>1900 && a<2022){
+//     alert(2022-a)
+// }
+// else{
+//     alert("Yilizi to`g`ri kiriting!")
+// }
+
+    // 21-masala
+
+// var a = prompt("Birinchi sonni kiriting:")-0;
+// var b = prompt("Ikkinchi sonni kiriting:")-0;
+// var c = prompt("Uchinchi sonni kiriting:")-0;
+// if(a <= b <= c){
+//     alert("a eng kichik son")
+// }
+// else{
+//     alert("false")
+// }
+
+    // 23-masala
+
+// var a = prompt("Birinchi sonni kiriting:")-0;
+// var b = prompt("Ikkinchi sonni kiriting:")-0;
+// var c = prompt("Uchinchi sonni kiriting:")-0;
+// if(a>0 && b>0 && c>0){
+//     alert("Sonlarni hammasi musbat")
+// }
+// else{
+//     alert("Orasida manfiy son qatnashgan")
+// }
+
+    // 24-masala
+// var a = prompt("Birinchi sonni kiriting:")-0;
+// var b = prompt("Ikkinchi sonni kiriting:")-0;
+// var c = prompt("Uchinchi sonni kiriting:")-0;
+// if(a>0 || b>0 || c>0){
+//     alert("sonlarni qaysi biridur yoki hammasi musbat")
+// }
+// else{
+//     alert("hammasi manfiy")
+// }
+
+
+
+
+
